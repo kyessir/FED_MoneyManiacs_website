e8c91cd45ef7ad918da3ae370351158061f8e12a
SianKim <siankim06@gmail.com>
2024-01-25 00:42:06 +0800

    added game page

---
+++ js/gamestart.js
@@ -0,0 +1,23 @@
+function openOverlay() {
+    document.getElementById('overlay').style.display = 'flex';
+}
+
+function closeOverlay() {
+    document.getElementById('overlay').style.display = 'none';
+}
+
+function openOverlay1() {
+    document.getElementById('overlay1').style.display = 'flex';
+}
+
+function closeOverlay1() {
+    document.getElementById('overlay1').style.display = 'none';
+}
+
+function openOverlay2() {
+    document.getElementById('overlay2').style.display = 'flex';
+}
+
+function closeOverlay2() {
+    document.getElementById('overlay2').style.display = 'none';
+}
